96945828ce1dd33e4b1f0b3b4bd2cfbe49c5266e
Qian Tang <qtang12@myseneca.ca>
2024-08-24 20:37:30 -0400

    Finished all

---
+++ script.js
@@ -0,0 +1,9 @@
+// navbar variables
+const navbarNav = document.querySelector(".navbar-nav");
+const navbarToggleBtn = document.querySelector(".nav-toggle-btn");
+
+// navbar toggle functionality
+navbarToggleBtn.addEventListener("click", function () {
+  navbarNav.classList.toggle("active");
+  this.classList.toggle("active");
+});
